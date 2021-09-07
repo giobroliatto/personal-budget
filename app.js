bc03674f2685d221d876f27d0863db74dad60d9d
gio broliatto <84979354+giobroliatto@users.noreply.github.com>
2021-09-07 02:08:31 -0300

    updates

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ class Expense {
     this.cost = cost
   }
 
+  // verificando se há campos em branco
   dataValidation() {
     for (let i in this) {
       if (this[i] == undefined || this[i] == '' || this[i] == null) {
@@ -18,6 +19,7 @@ class Expense {
   }
 }
 
+// criação do banco de dados local (local storage)
 class Db {
   constructor() {
     let id = localStorage.getItem('id')
@@ -38,6 +40,21 @@ class Db {
     localStorage.setItem(id, JSON.stringify(d))
 
     localStorage.setItem('id', id)
+  }
+
+  // selecionando todos os registros
+  allRecords() {
+    let expenses = []
+    let id = localStorage.getItem('id')
+
+    for (let i = 1; i <= id; i++) {
+      let expense = JSON.parse(localStorage.getItem(i))
+      if (expense === null) {
+        continue
+      }
+      expenses.push(expense)
+    }
+    return expenses
   }
 }
 
@@ -60,13 +77,77 @@ function registerExpense() {
     cost.value
   )
 
+  // registrando e trazendo a tela de sucesso ou erro de forma programática
   if (expense.dataValidation()) {
-    // db.engrave(expense)
-    $('#engraveSuccess').modal('show')
+    // sucesso
+    db.engrave(expense)
+    document.getElementById('modal-title').innerHTML = 'Registred!'
+    document.getElementById('modal-title-div').className =
+      'modal-header text-success'
+    document.getElementById('modal-content').innerHTML =
+      'Expense successfully added'
+    document.getElementById('modal-btn').innerHTML = 'Go back'
+    document.getElementById('modal-btn').className = 'btn btn-success'
+    $('#engraveRegister').modal('show')
   } else {
     // erro
-    $('#engraveError').modal('show')
+    document.getElementById('modal-title').innerHTML = 'Error'
+    document.getElementById('modal-title-div').className =
+      'modal-header text-danger'
+    document.getElementById('modal-content').innerHTML =
+      'Please, fill all the fields correctly'
+    document.getElementById('modal-btn').innerHTML = 'Go back and fix it'
+    document.getElementById('modal-btn').className = 'btn btn-danger'
+
+    $('#engraveRegister').modal('show')
   }
+}
+
+function loadExpenses() {
+  let expenses = []
+  expenses = db.allRecords()
+
+  // selecionando o tbody do html para criar a tabela
+  let expensesList = document.getElementById('expensesList')
+
+  /* 
+  <tr>
+    coluna 0 = <td>17/03/2021</td>
+    coluna 1 = <td>Food</td>
+    coluna 2 = <td>Month's buy</td>
+    coluna 3 = <td>450.50</td>
+    coluna n
+  </tr>
+     */
+
+  // percorrendo o array "expenses", listando cada despesa dinamicamente
+  expenses.forEach(function (d) {
+    // criando uma linha <tr>
+    let row = expensesList.insertRow()
+
+    // criando as colunas <td>
+    row.insertCell(0).innerHTML = `${d.day}/${d.month}/${d.year}`
+    switch (parseInt(d.type)) {
+      case 1:
+        d.type = 'Food'
+        break
+      case 2:
+        d.type = 'Education'
+        break
+      case 3:
+        d.type = 'Leisure'
+        break
+      case 4:
+        d.type = 'Health'
+        break
+      case 5:
+        d.type = 'Transportation'
+        break
+    }
+    row.insertCell(1).innerHTML = d.type
+    row.insertCell(2).innerHTML = d.description
+    row.insertCell(3).innerHTML = d.cost
+  })
 }
 
 /* // preparando o local storage para armazenar os registros de despesas (aqui o índice é fixo, ou seja, será alterado toda vez que for adicionado. é preciso criar um índice dinâmico)
